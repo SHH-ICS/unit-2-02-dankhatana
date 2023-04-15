17a43ac9cb46f5a6461cee5a91fee38f8ec4a9a0
dankhatana <124597966+dankhatana@users.noreply.github.com>
2023-04-15 02:16:46 +0000

    code

--- program.py
+++ program.py
@@ -1,5 +1,10 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction():
-  # My Function
+def myFunction(first_name):
+    return "Hello, {}!".format(first_name)
+
+
+first_name = input("Please enter your first name: ")
+
+print(myFunction(first_name))
